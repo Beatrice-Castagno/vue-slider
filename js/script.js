0deb3215b04e165ff2bc9942f28c94d8b7c3401d
Beatrice Castagno <beatrice.castagno1@gmail.com>
2021-03-25 17:17:40 +0100

    Aggiunto carosello con funzioni next e prev

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var app = new Vue(
+ {
+ el: '#root',
+ data: {
+ images: [
+   "img/Alfa-Romeo-Giulia-GTAm.jpg",
+   "img/aston-martin-v8-vantage-1977.jpg",
+   "img/chevrolet-camaro.jpg"
+  ],
+ imagesIndex: 0
+ },
+ methods: {
+   frecciaNext: function() {
+     this.imagesIndex++;
+
+     if (this.imagesIndex == this.images.length) {
+       this.imagesIndex = 0;
+     }
+   },
+
+   frecciaPrev: function() {
+     this.imagesIndex--;
+
+     if (this.imagesIndex < 0) {
+       this.imagesIndex = this.images.length - 1;
+     }
+   }
+ }
+}
+);
